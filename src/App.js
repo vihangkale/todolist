a998a6a87161fe94cd2f35ac749c16e27b0a87a6
vihang <vihangk64@gmail.com>
2022-05-05 10:22:11 +0530

    Add task and table view created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [fieldData, setFieldData] = useState([]);
+  const [updateFieldData, setUpdateFieldData] = useState([]);
+  let [fieldsData, setFieldsData] = useState([]);
+
+  useEffect(()=>{
+     fieldsData = [
+      {
+        name:"Name",
+        type:"text"
+      },
+      {
+        name:"Date",
+        type:"date"
+      },
+    ]
+
+    setFieldsData(fieldsData) 
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="table">
+
+      <h1>Add Task</h1>
+      <form>
+        <div className="display-flex flex-direction-column">
+          {fieldsData && fieldsData.map((field)=> (
+            <div key={field.name} className="display-flex flex-direction-column">
+              <label>{field.name}</label>
+              <input name={field.name} type={field.type}/> 
+            </div>
+            )
+          )}
+        </div>
+        <button className="m-t-10" type="submit" onClick={(e)=> e.preventDefault()}>Add Task</button>
+      </form>
+
+      <h1>Tasks</h1>
+        <table>
+          <thead>
+               <tr>
+                 <th>Task Done </th>
+                 <th>Name</th>
+                 <th>Date</th>
+               </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input type="checkbox"/>
+              </td>
+              <td>thtrhtr</td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   );
 }
