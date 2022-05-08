765f94e3d78ca224acc90d2f76f27f4054cb0ff1
vihang <vihangk64@gmail.com>
2022-05-08 13:53:45 +0530

    Working TodoList with CRUD functionality working

--- src/App.js
+++ src/App.js
@@ -1,61 +1,131 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [fieldData, setFieldData] = useState([]);
-  const [updateFieldData, setUpdateFieldData] = useState([]);
-  let [fieldsData, setFieldsData] = useState([]);
+  const [addFieldData, setAddFieldData] = useState({ name: "", date: "" });
+  const [Tasks, setTasks] = useState([]);
+  const [isEditTaskData, setEditTaskData] = useState(false);
 
-  useEffect(()=>{
-     fieldsData = [
-      {
-        name:"Name",
-        type:"text"
-      },
-      {
-        name:"Date",
-        type:"date"
-      },
-    ]
+  useEffect(() => {
+    getTaskData();
+  }, []);
 
-    setFieldsData(fieldsData) 
+  function getTaskData() {
+    const tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
+    setTasks(tasks);
+  }
 
-  },[])
+  async function addTask(e) {
+    const tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
+    addFieldData.id = uuidv4();
+    setTasks([...tasks, addFieldData]);
+    localStorage.setItem("Tasks", JSON.stringify([...tasks, addFieldData]));
+  }
 
+  function inputData(e) {
+    e.preventDefault();
+    setAddFieldData({ ...addFieldData, [e.target.name]: e.target.value });
+  }
+  function editTaskData(task) {
+    setEditTaskData(true);
+    setAddFieldData(task);
+  }
+
+  function DeleteTask(key) {
+    const tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
+    tasks.map((task, i) => {
+      if (task.id === key.id) {
+        tasks.splice(i, 1);
+      }
+    });
+    localStorage.setItem("Tasks", JSON.stringify(tasks));
+    getTaskData();
+  }
+
+  function updateTask() {
+    const tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
+    tasks.map((task) => {
+      if (task.id === addFieldData.id) {
+        task = Object.assign(task, addFieldData);
+      }
+    });
+    localStorage.setItem("Tasks", JSON.stringify(tasks));
+    getTaskData();
+  }
+
+  function clearTaskData() {
+    localStorage.clear();
+    getTaskData();
+  }
   return (
     <div className="table">
+      <h1>{isEditTaskData ? "Update Task" : "Add Task"}</h1>
+      <div className="display-flex flex-direction-column">
+        <label>Name</label>
+        <input
+          name="name"
+          type="text"
+          value={addFieldData.name}
+          onChange={(e) => inputData(e)}
+        />
+        <label>Date</label>
+        <input
+          name="date"
+          type="date"
+          value={addFieldData.date}
+          onChange={(e) => inputData(e)}
+        />
+      </div>
+      <button
+        className="m-t-10"
+        type="submit"
+        onClick={(e) => {
+          isEditTaskData ? updateTask() : addTask(e);
+        }}
+      >
+        {isEditTaskData ? "Update Task" : "Add Task"}
+      </button>
 
-      <h1>Add Task</h1>
-      <form>
-        <div className="display-flex flex-direction-column">
-          {fieldsData && fieldsData.map((field)=> (
-            <div key={field.name} className="display-flex flex-direction-column">
-              <label>{field.name}</label>
-              <input name={field.name} type={field.type}/> 
-            </div>
-            )
-          )}
-        </div>
-        <button className="m-t-10" type="submit" onClick={(e)=> e.preventDefault()}>Add Task</button>
-      </form>
-
-      <h1>Tasks</h1>
-        <table>
-          <thead>
-               <tr>
-                 <th>Task Done </th>
-                 <th>Name</th>
-                 <th>Date</th>
-               </tr>
-          </thead>
-          <tbody>
+      <div class="display-flex align-center">
+        <h1>Tasks</h1>
+        <button class="m-l-10" onClick={(e) => clearTaskData()}>
+          Clear All Tasks
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {Tasks.map((task) => (
             <tr>
+              <td>{task.date}</td>
+              <td>{task.name}</td>
               <td>
-                <input type="checkbox"/>
+                <button
+                  onClick={(e) => {
+                    isEditTaskData
+                      ? setEditTaskData(false)
+                      : editTaskData(task);
+                  }}
+                >
+                  {task.id === addFieldData.id && isEditTaskData
+                    ? "Stop Edit"
+                    : "Edit"}
+                </button>
               </td>
-              <td>thtrhtr</td>
+              <td>
+                <button onClick={(e) => DeleteTask(task)}>Delete</button>
+              </td>
             </tr>
-          </tbody>
-        </table>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
